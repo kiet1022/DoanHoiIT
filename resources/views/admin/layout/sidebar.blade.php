e584d8552e2e06c80e0aa17aa6525f630fbee3a7
kiet1022 <kiet1022@gmail.com>
2019-03-07 00:15:23 +0700

    Edit Bí thư to P. Bí Thư

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -5,7 +5,7 @@
       <div class="sidebar-brand-icon rotate-n-15">
         <i class="fas fa-laugh-wink"></i>
       </div>
-      <div class="sidebar-brand-text mx-3">THƯỜNG TRỰC <sup>(Bí thư)</sup></div>
+      <div class="sidebar-brand-text mx-3">THƯỜNG TRỰC <sup>(P. Bí thư)</sup></div>
     </a>
 
     <!-- Divider -->
