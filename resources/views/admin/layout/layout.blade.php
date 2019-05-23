af097d10d36aeb166e74da9ca1b98d1eed64218c
kiet1022 <kiet1022@gmail.com>
2019-05-23 23:11:30 +0700

    update icon

--- resources/views/admin/layout/layout.blade.php
+++ resources/views/admin/layout/layout.blade.php
@@ -11,7 +11,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   {{-- <base href="{{asset('')}}"> --}}
   <title>@yield('title')</title>
-  <link rel="shortcut icon" href="{{asset('images/icon.ico')}}"> 
+  <link rel="shortcut icon" href="{{asset('assets/img/faculty_banner.png')}}"> 
   <!-- Custom fonts for this template-->
   <link href="{{asset('assets/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
