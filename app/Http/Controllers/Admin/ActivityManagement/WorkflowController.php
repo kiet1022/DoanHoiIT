1d868739700dabae998585cdcbbb0c2c14892c12
kiet1022 <kiet1022@gmail.com>
2019-07-08 22:21:37 +0700

    fix edit workflow

--- app/Http/Controllers/Admin/ActivityManagement/WorkflowController.php
+++ app/Http/Controllers/Admin/ActivityManagement/WorkflowController.php
@@ -22,186 +22,188 @@ use App\Models\Log;
 
 class WorkflowController extends Controller
 {
-  /**
-  * Get add new workflow page
-  * 
-  */
-  public function getAddAcWorkFlow(Request $req, $id = null){
-    // Check user role
-		$req->user()->authorizeRoles([config('constants.FULL_ROLES'), config('constants.ACTIVITY_MANAGE_ROLE')]);
-    if($id != 'null'){
-      $this->data['acid'] = $id;
-    }
-    $this->data['activities'] = Activity::where('year','2018 - 2019')->get();
-    $this->data['year'] = SchoolYear::where('type',1)->orderBy('name','desc')->get();
-    $this->data['students'] = Student::whereHas('execComm')->orWhereHas('association')->orWhereHas('collaborator')->get();
-    return view('admin.workflow.add_workflow')->with($this->data);
-  }
-  
-  /**
-  * Add new workflow
-  * 
-  * @param Request $req The request that user sent
-  */
-  public function postAddAcWorkFlow(Request $req){
-    // Check user role
-		$req->user()->authorizeRoles([config('constants.FULL_ROLES'), config('constants.ACTIVITY_MANAGE_ROLE')]);
-    try {
-      DB::beginTransaction();
-      for($i = 0; $i < count($req->leader_) ; $i++){
-        $workflow = new WorkFlow;
-        $workflow->activity_id = $req->activity;
-        $workflow->student_id = $req->leader_[$i];
-        $workflow->content = $req->workcontent_[$i];
-        $workflow->deadline = DateTimeUtil::convertToYmd($req->deadline_[$i]);
-        $workflow->created_by = Auth::user()->id;
-        $workflow->progress = 0;
-        $workflow->save();
-      }
-      DB::commit();
-      return redirect()->back()->with(config('constants.SUCCESS'),'Thêm công việc thành công!');
-    } catch (Exception $ex) {
-      DB::rollback();
-      return redirect()->back()->with(config('constants.ERROR'),'Thêm công việc thất bại!');
-    }
-  }
-  
-  /**
-  * Get work flow list of an activity
-  * 
-  * @param Integer $id The id of activity
-  * 
-  */
-  public function getListWorkFlow(Request $req, $id = null){
-    // Check user role
-		$req->user()->authorizeRoles([config('constants.FULL_ROLES'), config('constants.ACTIVITY_MANAGE_ROLE')]);
-    $this->data['workflows'] = WorkFlow::with(['details','ofStudent','ofActivity'])->where('activity_id',$id)->get();
-    // return $this->data['workflows'];
-    return view('admin.workflow.workflow_list')->with($this->data);
-  }
-  
-  /**
-  * Get detail of activity workflow
-  * 
-  * @param Request $req The request that user sent
-  * 
-  */
-  public function getWorkFlowDetail(Request $req){
-    $this->data['workflowDetail'] = $req->content;
-    // return $this->data['workflowDetail'];
-    return response()->view('admin.workflow.modal_workflow_detail', $this->data);
-  }
-  
-  /**
-  * Edit workflow detail
-  * 
-  * @param Request $req the request that user sent
-  * 
-  */
-  public function postEditWorkFlowDetail(Request $req){
-    // Check user role
-		$req->user()->authorizeRoles([config('constants.FULL_ROLES'), config('constants.ACTIVITY_MANAGE_ROLE')]);
-    $wfdetail = WorkFlow::where('id',$req->id)->with(['details'])->first();
-    $arrDetailId = [];
-    foreach ($wfdetail->details as $detail) {
-      array_push($arrDetailId, $detail->id);
+    /**
+    * Get add new workflow page
+    * 
+    */
+    public function getAddAcWorkFlow(Request $req, $id = null){
+        // Check user role
+        $req->user()->authorizeRoles([config('constants.FULL_ROLES'), config('constants.ACTIVITY_MANAGE_ROLE')]);
+        if($id != 'null'){
+            $this->data['acid'] = $id;
+        }
+        $this->data['activities'] = Activity::where('year','2018 - 2019')->get();
+        $this->data['year'] = SchoolYear::where('type',1)->orderBy('name','desc')->get();
+        $this->data['students'] = Student::whereHas('execComm')->orWhereHas('association')->orWhereHas('collaborator')->get();
+        return view('admin.workflow.add_workflow')->with($this->data);
     }
     
-    try {
-      DB::beginTransaction();
-      $wfdetail->content = $req->contentDetail;
-      $wfdetail->updated_by = Auth::user()->id;
-      
-      // Delete funding detail if it is not exist
-      foreach ($arrDetailId as $detailid) {
-        if(!in_array(intval($detailid), $req->workflowId_)){
-          $deleteDetail = WorkFlowDetail::find($detailid);
-          $deleteDetail->forceDelete();
+    /**
+    * Add new workflow
+    * 
+    * @param Request $req The request that user sent
+    */
+    public function postAddAcWorkFlow(Request $req){
+        // Check user role
+        $req->user()->authorizeRoles([config('constants.FULL_ROLES'), config('constants.ACTIVITY_MANAGE_ROLE')]);
+        try {
+            DB::beginTransaction();
+            for($i = 0; $i < count($req->leader_) ; $i++){
+                $workflow = new WorkFlow;
+                $workflow->activity_id = $req->activity;
+                $workflow->student_id = $req->leader_[$i];
+                $workflow->content = $req->workcontent_[$i];
+                $workflow->deadline = DateTimeUtil::convertToYmd($req->deadline_[$i]);
+                $workflow->created_by = Auth::user()->id;
+                $workflow->progress = 0;
+                $workflow->save();
+            }
+            DB::commit();
+            return redirect()->back()->with(config('constants.SUCCESS'),'Thêm công việc thành công!');
+        } catch (Exception $ex) {
+            DB::rollback();
+            return redirect()->back()->with(config('constants.ERROR'),'Thêm công việc thất bại!');
         }
-      }
-      
-      // update detail
-      for($i = 0; $i < count($req->content_); $i++){
-        if($req->workflowId_[$i] == 0){
-          $newDetail = new WorkFlowDetail;
-          $newDetail->workflow_id = $req->id;
-          $newDetail->content = $req->content_[$i];
-          $newDetail->progress = $req->progress_[$i];
-          $newDetail->created_by = Auth::user()->id;
-          $newDetail->save();
-        } else {
-          $oldDetail = WorkFlowDetail::find($req->workflowId_[$i]);
-          $oldDetail->content = $req->content_[$i];
-          $oldDetail->progress = $req->progress_[$i];
-          $oldDetail->created_by = Auth::user()->id;
-          $oldDetail->save();
-          
+    }
+    
+    /**
+    * Get work flow list of an activity
+    * 
+    * @param Integer $id The id of activity
+    * 
+    */
+    public function getListWorkFlow(Request $req, $id = null){
+        // Check user role
+        $req->user()->authorizeRoles([config('constants.FULL_ROLES'), config('constants.ACTIVITY_MANAGE_ROLE')]);
+        $this->data['workflows'] = WorkFlow::with(['details','ofStudent','ofActivity'])->where('activity_id',$id)->get();
+        // return $this->data['workflows'];
+        return view('admin.workflow.workflow_list')->with($this->data);
+    }
+    
+    /**
+    * Get detail of activity workflow
+    * 
+    * @param Request $req The request that user sent
+    * 
+    */
+    public function getWorkFlowDetail(Request $req){
+        $this->data['workflowDetail'] = $req->content;
+        // return $this->data['workflowDetail'];
+        return response()->view('admin.workflow.modal_workflow_detail', $this->data);
+    }
+    
+    /**
+    * Edit workflow detail
+    * 
+    * @param Request $req the request that user sent
+    * 
+    */
+    public function postEditWorkFlowDetail(Request $req){
+        // Check user role
+        $req->user()->authorizeRoles([config('constants.FULL_ROLES'), config('constants.ACTIVITY_MANAGE_ROLE')]);
+        $wfdetail = WorkFlow::where('id',$req->id)->with(['details','ofStudent','ofActivity'])->first();
+        $arrDetailId = [];
+        foreach ($wfdetail->details as $detail) {
+            array_push($arrDetailId, $detail->id);
         }
-      }
-      
-      // update workflow progress after update of inser detail
-      $percent = DB::table('workflow_details')->select(DB::raw('sum(progress) / count(*) as percent'))->where('workflow_id', $req->id)
-      ->whereNull('deleted_at')->first();
-      $wfdetail->progress = intval($percent->percent);
-      $wfdetail->save();
-      DB::commit();
-      return redirect()->back()->with(config('constants.SUCCESS'),'Cập nhật công việc thành công');
-    } catch (Excetion $ex) {
-      DB::rollback();
-      return redirect()->back()->with(config('constants.ERROR'),'Cập nhật công việc thất bại!');
+        
+        try {
+            DB::beginTransaction();
+            $wfdetail->content = $req->contentDetail;
+            $wfdetail->updated_by = Auth::user()->id;
+            
+            // Delete funding detail if it is not exist
+            foreach ($arrDetailId as $detailid) {
+                if(!in_array(intval($detailid), $req->workflowId_)){
+                    $deleteDetail = WorkFlowDetail::find($detailid);
+                    $deleteDetail->forceDelete();
+                }
+            }
+            
+            // update detail
+            for($i = 0; $i < count($req->content_); $i++){
+                if($req->workflowId_[$i] == 0){
+                    $newDetail = new WorkFlowDetail;
+                    $newDetail->workflow_id = $req->id;
+                    $newDetail->content = $req->content_[$i];
+                    $newDetail->progress = $req->progress_[$i];
+                    $newDetail->created_by = Auth::user()->id;
+                    $newDetail->save();
+                } else {
+                    $oldDetail = WorkFlowDetail::find($req->workflowId_[$i]);
+                    $oldDetail->content = $req->content_[$i];
+                    $oldDetail->progress = $req->progress_[$i];
+                    $oldDetail->created_by = Auth::user()->id;
+                    $oldDetail->save();
+                    
+                }
+            }
+            
+            // update workflow progress after update of inser detail
+            $percent = DB::table('workflow_details')->select(DB::raw('sum(progress) / count(*) as percent'))->where('workflow_id', $req->id)
+            ->whereNull('deleted_at')->first();
+            $wfdetail->progress = intval($percent->percent);
+            $wfdetail->save();
+            DB::commit();
+            $this->data['workflows'] = WorkFlow::with(['details','ofStudent','ofActivity'])->where('activity_id',$wfdetail->ofActivity->id)->get();
+            return redirect()->back()->with(config('constants.SUCCESS'),'Cập nhật công việc thành công')->with('workflows',$this->data['workflows']);
+        } catch (Excetion $ex) {
+            DB::rollback();
+            return redirect()->back()->with(config('constants.ERROR'),'Cập nhật công việc thất bại!');
+        }
     }
-  }
-  
-  /**
-  * Detlete workflow
-  * 
-  * @param Integer $id The id of workflow
-  * 
-  */
-  public function deleteWorkFlow(Request $req, $id){
-    // Check user role
-		$req->user()->authorizeRoles([config('constants.FULL_ROLES'), config('constants.ACTIVITY_MANAGE_ROLE')]);
-    $workflow = WorkFlow::with(['details'])->find($id);
-    foreach ($workflow->details as $detail) {
-      $detail->delete();
+    
+    /**
+    * Detlete workflow
+    * 
+    * @param Integer $id The id of workflow
+    * 
+    */
+    public function deleteWorkFlow(Request $req, $id){
+        // Check user role
+        $req->user()->authorizeRoles([config('constants.FULL_ROLES'), config('constants.ACTIVITY_MANAGE_ROLE')]);
+        $workflow = WorkFlow::with(['details'])->find($id);
+        foreach ($workflow->details as $detail) {
+            $detail->delete();
+        }
+        $workflow->delete();
+        return redirect()->back()->with(config('constants.SUCCESS'),'Xóa công việc thành công');
     }
-    $workflow->delete();
-    return redirect()->back()->with(config('constants.SUCCESS'),'Xóa công việc thành công');
-  }
-
-  /**
-  * Get work flow list
-  * 
-  * @param Integer $id The id of activity
-  * 
-  */
-  public function getWorkFlowList(Request $req){
-    // Check user role
-		$req->user()->authorizeRoles([config('constants.FULL_ROLES'), config('constants.ACTIVITY_MANAGE_ROLE')]);
-    // delete session if exist
-    session()->forget('_old_input');
-    if($req->user()->hasRole(config('constants.FULL_ROLES'))){
-      $this->data['activities'] = Activity::all();
-    } else if ($req->user()->hasRole(config('constants.ACTIVITY_MANAGE_ROLE'))){
-      $this->data['activities'] = Activity::where('leader',Auth::user()->student->student_id)->get();
+    
+    /**
+    * Get work flow list
+    * 
+    * @param Integer $id The id of activity
+    * 
+    */
+    public function getWorkFlowList(Request $req){
+        // Check user role
+        $req->user()->authorizeRoles([config('constants.FULL_ROLES'), config('constants.ACTIVITY_MANAGE_ROLE')]);
+        // delete session if exist
+        session()->forget('_old_input');
+        if($req->user()->hasRole(config('constants.FULL_ROLES'))){
+            $this->data['activities'] = Activity::all();
+        } else if ($req->user()->hasRole(config('constants.ACTIVITY_MANAGE_ROLE'))){
+            $this->data['activities'] = Activity::where('leader',Auth::user()->student->student_id)->get();
+        }
+        return view('admin.workflow.list_workflow')->with($this->data);
     }
-    return view('admin.workflow.list_workflow')->with($this->data);
-  }
-
-  /**
-   * Filter workflow by id activity
-   * 
-   * @param Request $req The request that user sent
-   * 
-   */
-  public function filterWorkFlow(Request $req){
-    if($req->user()->hasRole(config('constants.FULL_ROLES'))){
-      $this->data['activities'] = Activity::all();
-    } else if ($req->user()->hasRole(config('constants.ACTIVITY_MANAGE_ROLE'))){
-      $this->data['activities'] = Activity::where('leader',Auth::user()->student->student_id)->get();
+    
+    /**
+    * Filter workflow by id activity
+    * 
+    * @param Request $req The request that user sent
+    * 
+    */
+    public function filterWorkFlow(Request $req){
+        // return $req->all();
+        if($req->user()->hasRole(config('constants.FULL_ROLES'))){
+            $this->data['activities'] = Activity::all();
+        } else if ($req->user()->hasRole(config('constants.ACTIVITY_MANAGE_ROLE'))){
+            $this->data['activities'] = Activity::where('leader',Auth::user()->student->student_id)->get();
+        }
+        $this->data['workflows'] = WorkFlow::with(['details','ofStudent','ofActivity'])->where('activity_id',$req->activity)->get();
+        $req->flash();
+        return view('admin.workflow.list_workflow')->with($this->data);
     }
-    $this->data['workflows'] = WorkFlow::with(['details','ofStudent','ofActivity'])->where('activity_id',$req->activity)->get();
-    $req->flash();
-    return view('admin.workflow.list_workflow')->with($this->data);
-  }
 }
--- resources/views/admin/workflow/list_workflow.blade.php
+++ resources/views/admin/workflow/list_workflow.blade.php
@@ -15,105 +15,153 @@
             </ol>
         </nav>
     </div>
-  <div class="row">
-    <div class="col page-title-header">
-      <h4>Tiến độ công việc</h4>
-    </div>
-  </div>
-  <div class="row">
-      <div class="col-md-12 col-sm-12 col-xs-12 custom_panel">
-          <form action="{{ route('get_workflow_filter') }}" method="POST">
-            @csrf
-            <div class="form-row">
-            {{-- Activity --}}
-            <div class="form-group col-4 offset-md-4">
-                <label for="activity" class="col-md-12 common-label-inline">Chương trình:</label>
-                <div class="col-md-12 col-sm-12 col-xs-12 px-0 d-flex">
-                  <select name="activity" id="activity" class="form-control col-md-8 col-sm-8 col-xs-8">
-                    <option value="">Chọn chương trình</option>
-                    @foreach ($activities as $a)
-                    <option value="{{ $a->id }}" {!! changeSelectedStatus("$a->id", old('activity') ) !!} >{{ $a->name }}</option>
-                    @endforeach
-                  </select>
-                  <button class="btn btn-primary ml-3" type="submit">Xem tiến độ</button>
-                </div>
-              </div>
-            </div>
-          </form>
+    <div class="row">
+        <div class="col page-title-header">
+            <h4>Tiến độ công việc</h4>
         </div>
-  </div>
-  @if (isset($workflows))
+    </div>
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12 custom_panel">
+            <form action="{{ route('get_workflow_filter') }}" method="POST">
+                @csrf
+                <div class="form-row">
+                    {{-- Activity --}}
+                    <div class="form-group col-4 offset-md-4">
+                        <label for="activity" class="col-md-12 common-label-inline">Chương trình:</label>
+                        <div class="col-md-12 col-sm-12 col-xs-12 px-0 d-flex">
+                            <select name="activity" id="activity" class="form-control col-md-8 col-sm-8 col-xs-8">
+                                <option value="">Chọn chương trình</option>
+                                @foreach ($activities as $a)
+                                <option value="{{ $a->id }}" {!! changeSelectedStatus("$a->id", old('activity') ) !!} 
+                                    @if(session('workflows')){!! changeSelectedStatus("$a->id", session('workflows')[0]->ofActivity->id."" ) !!} @endif >{{ $a->name }}</option>
+                                @endforeach
+                            </select>
+                            <button class="btn btn-primary ml-3" type="submit">Xem tiến độ</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    @if (isset($workflows))
     @if(count($workflows) > 0)
-  <div class="row custom_panel">
-    
-    <div class="col-md-12 col-sm-12 col-xs-12 d-flex flex-wrap p-0" id="in-card-content">
-      @foreach ($workflows as $workflow)
-      <div class="col-4">
-        <div class="card" style="height: 100%">
-          <div class="card-header">
-            <h5 class="card-title" style="color:red;">{{ $workflow->ofStudent->name }}</h5>
-          </div>
-          <div class="card-body">
-            <p class="card-text"><h5 class="text-primary">{{ $workflow->content }}</h5></p>
-            <ul class="list-group list-group-flush">
-              @foreach ($workflow->details as $detail)
-              <li class="list-group-item">
-                {{ $detail->content }}
-                <div class="progress">
-                  <div class="progress-bar {{ changeProgressbarColor($detail->progress) }}" role="progressbar" aria-valuenow="{{ $detail->progress }}" aria-valuemin="0" aria-valuemax="{{ $detail->progress }}" style='width:{{ $detail->progress}}%'>{{ $detail->progress }}%</div>
+    <div class="row custom_panel">
+        
+        <div class="col-md-12 col-sm-12 col-xs-12 d-flex flex-wrap p-0" id="in-card-content">
+            @foreach ($workflows as $workflow)
+            <div class="col-4">
+                <div class="card" style="height: 100%">
+                    <div class="card-header">
+                        <h5 class="card-title" style="color:red;">{{ $workflow->ofStudent->name }}</h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text"><h5 class="text-primary">{{ $workflow->content }}</h5></p>
+                        <ul class="list-group list-group-flush">
+                            @foreach ($workflow->details as $detail)
+                            <li class="list-group-item">
+                                {{ $detail->content }}
+                                <div class="progress">
+                                    <div class="progress-bar {{ changeProgressbarColor($detail->progress) }}" role="progressbar" aria-valuenow="{{ $detail->progress }}" aria-valuemin="0" aria-valuemax="{{ $detail->progress }}" style='width:{{ $detail->progress}}%'>{{ $detail->progress }}%</div>
+                                </div>
+                            </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    <div class="card-footer">
+                        @if ($workflow->student_id === Auth::user()->student->student_id)
+                        <div class="d-flex m-1">
+                            <a href="#" class="btn btn-primary btn-sm detail mr-1" style="width:50%" data-content="{{$workflow}}"><i class="fas fa-edit"></i> Chỉnh sửa</a>
+                            <a href="{{ route('delete_workflow',['id'=>$workflow->id])}}" class="btn btn-danger btn-sm ml-1" style="width:50%" "><i class="fas fa-minus"></i> Xóa</a>
+                        </div>
+                        @endif
+                    </div>
                 </div>
-              </li>
-              @endforeach
-            </ul>
-          </div>
-          <div class="card-footer">
-            @if ($workflow->student_id === Auth::user()->student->student_id)
-            <div class="d-flex m-1">
-                <a href="#" class="btn btn-primary btn-sm detail mr-1" style="width:50%" data-content="{{$workflow}}"><i class="fas fa-edit"></i> Chỉnh sửa</a>
-                <a href="{{ route('delete_workflow',['id'=>$workflow->id])}}" class="btn btn-danger btn-sm ml-1" style="width:50%" "><i class="fas fa-minus"></i> Xóa</a>
             </div>
-            @endif
-          </div>
+            @endforeach
         </div>
-      </div>
-      @endforeach
     </div>
-  </div>
     @else
     <div class="row custom_panel text-center">
         <div class="col-md-12 col-sm-12 col-xs-12 p-0" id="in-card-content">
-          <span class="badge badge-warning">Chương trình này chưa được phân công công việc</span>
+            <span class="badge badge-warning">Chương trình này chưa được phân công công việc</span>
         </div>
     </div>
     @endif
-  @endif
+    @endif
+    
+    {{-- Hiển thị lại chi tiết công việc sau khi edit --}}
+    @if(session('workflows'))
+    @if(count(session('workflows')) > 0)
+    <div class="row custom_panel">
+        
+        <div class="col-md-12 col-sm-12 col-xs-12 d-flex flex-wrap p-0" id="in-card-content">
+            @foreach (session('workflows') as $workflow)
+            <div class="col-4">
+                <div class="card" style="height: 100%">
+                    <div class="card-header">
+                        <h5 class="card-title" style="color:red;">{{ $workflow->ofStudent->name }}</h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text"><h5 class="text-primary">{{ $workflow->content }}</h5></p>
+                        <ul class="list-group list-group-flush">
+                            @foreach ($workflow->details as $detail)
+                            <li class="list-group-item">
+                                {{ $detail->content }}
+                                <div class="progress">
+                                    <div class="progress-bar {{ changeProgressbarColor($detail->progress) }}" role="progressbar" aria-valuenow="{{ $detail->progress }}" aria-valuemin="0" aria-valuemax="{{ $detail->progress }}" style='width:{{ $detail->progress}}%'>{{ $detail->progress }}%</div>
+                                </div>
+                            </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    <div class="card-footer">
+                        @if ($workflow->student_id === Auth::user()->student->student_id)
+                        <div class="d-flex m-1">
+                            <a href="#" class="btn btn-primary btn-sm detail mr-1" style="width:50%" data-content="{{$workflow}}"><i class="fas fa-edit"></i> Chỉnh sửa</a>
+                            <a href="{{ route('delete_workflow',['id'=>$workflow->id])}}" class="btn btn-danger btn-sm ml-1" style="width:50%" "><i class="fas fa-minus"></i> Xóa</a>
+                        </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+    @else
+    <div class="row custom_panel text-center">
+        <div class="col-md-12 col-sm-12 col-xs-12 p-0" id="in-card-content">
+            <span class="badge badge-warning">Chương trình này chưa được phân công công việc</span>
+        </div>
+    </div>
+    @endif
+    @endif
 </div>
 {{-- Breadcrumb --}}
 <div class="col-12">
-  <nav aria-label="breadcrumb">
-    <ol class="breadcrumb cm-breadcrumb">
-      <li class="breadcrumb-item"><a href="{{ URL::previous() }}" class="cm-breadcrumb-a"><i class="fas fa-arrow-circle-left"></i> Quay lại</a></li>
-      {{-- <li class="breadcrumb-item active" aria-current="page">Chỉnh sửa thông tin</li> --}}
-    </ol>
-  </nav>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb cm-breadcrumb">
+            <li class="breadcrumb-item"><a href="{{ URL::previous() }}" class="cm-breadcrumb-a"><i class="fas fa-arrow-circle-left"></i> Quay lại</a></li>
+            {{-- <li class="breadcrumb-item active" aria-current="page">Chỉnh sửa thông tin</li> --}}
+        </ol>
+    </nav>
 </div>
 <!-- Modal -->
 <div class="modal animated jackInTheBox" id="workflowDetail" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  
+    
 </div>
 @endsection
 @section('js')
 <!-- Page level plugins -->
 <script>
-  var BASE_URL = "{{ asset('admin/activities/workflow') }}";
-  @if(session('error'))
-  var error = "{{session('error')}}";
-  showNotify('error',error);
-  @endif
-  @if(session('success'))
-  var success = "{{session('success')}}";
-  showNotify('success',success);
-  @endif
+    var BASE_URL = "{{ asset('admin/activities/workflow') }}";
+    @if(session('error'))
+    var error = "{{session('error')}}";
+    showNotify('error',error);
+    @endif
+    @if(session('success'))
+    var success = "{{session('success')}}";
+    showNotify('success',success);
+    @endif
 </script>
 <script src="{{asset('assets\js\admin\workflow\workflow_list.js')}}"></script>
 @endsection
